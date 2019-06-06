079905c6bb0393611d02a72e810f78533e4acd07
mlundvall <maria.v.lundvall@gmail.com>
2019-06-06 14:22:02 -0700

    Update User.py

--- DreamTeam_Code/User.py
+++ DreamTeam_Code/User.py
@@ -2,7 +2,10 @@
     The purpose of this class is to provide images from user into image list for regional layer.
 """
 class User:
-    images = []
+    # userImage is a 2D array that stores image data (pixel values)
+    userImage = [][]
+    imageList = []
+    ItemsLibrary = []
 
     """
     Enable user to provide multiple images.
